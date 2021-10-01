9f474429b0e86574f0a877a016a5c260a8c0aa4d
jair <prs079107@gmail.com>
2021-10-01 18:24:33 -0500

    Carrito de compra

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@ const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const listaCursos = document.querySelector("#lista-cursos");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 
+let carritoCompras = [];
+
+const aumentarCantidadCurso = (indexCurso) => {
+  carritoCompras[indexCurso].cantidad += 1;
+};
+
+const disminuirCacntidadCurso = (indexCurso) => {
+  console.log(indexCurso);
+};
+
 const leerDatosCurso = (curso) => {
   const infoCurso = {
     imagen: curso.querySelector("img").src,
@@ -12,7 +22,48 @@ const leerDatosCurso = (curso) => {
     cantidad: 1,
   };
 
-  console.log(infoCurso);
+  return infoCurso;
+};
+
+const limpiarHTML = () => {
+  contenedorCarrito.innerHTML = "";
+};
+
+const actualizarDOM = () => {
+  limpiarHTML();
+
+  carritoCompras.forEach((curso) => {
+    const { imagen, titulo, precio, id, cantidad } = curso;
+
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>
+        <img src="${imagen}" width="100" />
+      </td>
+      <td>${titulo}</td>
+      <td>${precio}</td>
+      <td>${cantidad}</td>
+      <td>
+        <a href="#" class="borrar-curso" data-id="${id}">X</a>
+      </td>
+    `;
+
+    contenedorCarrito.appendChild(row);
+  });
+};
+
+const agregarCarrito = (infoCurso) => {
+  const indexCurso = carritoCompras.findIndex(
+    (curso) => curso.id === infoCurso.id
+  );
+
+  if (indexCurso !== -1) {
+    aumentarCantidadCurso(indexCurso);
+  } else {
+    carritoCompras = [...carritoCompras, infoCurso];
+  }
+
+  actualizarDOM();
 };
 
 const agregarCurso = (e) => {
@@ -20,12 +71,32 @@ const agregarCurso = (e) => {
 
   if (e.target.classList.contains("agregar-carrito")) {
     const cursoSeleccionado = e.target.parentElement.parentElement;
-    leerDatosCurso(cursoSeleccionado);
+    const infoCurso = leerDatosCurso(cursoSeleccionado);
+    agregarCarrito(infoCurso);
+  }
+};
+
+const borrarCursoDeLista = (idCurso) => {
+  carritoCompras = carritoCompras.filter((curso) => curso.id !== idCurso);
+};
+
+const eliminarCurso = (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("borrar-curso")) {
+    const idCurso = e.target.getAttribute("data-id");
+
+    borrarCursoDeLista(idCurso);
+    actualizarDOM();
   }
 };
 
 const cargarEventListeners = () => {
   listaCursos.addEventListener("click", agregarCurso);
+
+  contenedorCarrito.addEventListener("click", eliminarCurso);
+
+  vaciarCarritoBtn.addEventListener("click", limpiarHTML);
 };
 
 cargarEventListeners();
